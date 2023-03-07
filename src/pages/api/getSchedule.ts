e6631fd3571650171500fd225d27f22571e1cb17
Sharlotte <aaa9810321@gmail.com>
2023-03-07 20:56:38 +0900

    feat: add getSchedule api

--- src/pages/api/getSchedule.ts
+++ src/pages/api/getSchedule.ts
@@ -1,6 +1,6 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
-import puppeteer, { HTTPResponse } from "puppeteer";
-import fs from 'fs';
+import puppeteer, { type Page } from "puppeteer";
+import domtoimage from 'dom-to-image';
 
 const KU_URL = "https://sugang.kyungnam.ac.kr";
 
@@ -36,18 +36,31 @@ export default async function handler(
   console.log("page change completed: ", page.url());
 
   await page.click("table tr:nth-child(3) a");
-  const hrefHandle = await page
+  const hrefUrl = await page
     .$("a[onclick=\"OnMenu(20)\"]")
     .then(handle => handle?.getProperty("href"))
     .then(prop => prop?.toString().split("JSHandle:")[1]);
-  if(!hrefHandle) throw new Error("cannot find href!");
+  if(!hrefUrl) throw new Error("cannot find href!");
 
-  await page.goto(hrefHandle.toString());
+  await page.goto(hrefUrl);
+  await page.waitForNavigation({ timeout: 1000000000 });
+
   console.log("page change completed: ", page.url());
+  
+  const FrameDOM = await page.$("#ReportViewerControl1_ucReportViewer1_ContentFrame")
+    .then(handle => handle?.jsonValue());
+  if(!FrameDOM) throw new Error("cannot find FrameDOM!");
 
-  await page.("#ReportViewerControl1_ucReportToolbar1_Menu_ITCNT13_SaveFormat_I", "Image");
-  await page.click("#ReportViewerControl1_ucReportToolbar1_Menu_DXI12_Img");
-  console.log("pages: ", browser.pages.length);
+  const url = await domtoimage.toPng(FrameDOM);
+  console.log("dom2image completed! - ", url);
+  res.status(200).send(JSON.stringify({ url }));
+
+  /*
+  //await page.("#ReportViewerControl1_ucReportToolbar1_Menu_ITCNT13_SaveFormat_I", "Image");
+  const popup = await new Promise<Page>(resolve => browser.once('targetcreated', target => resolve(target.page())));
+  await page.click("#ReportViewerControl1_ucReportToolbar1_Menu_DXI12_Img"); 
+  console.log(await popup.$("img").then(res => res?.jsonValue()));
+  
   const buffer = await new Promise<HTTPResponse>(res => {
     page.on('response', async response => {
     if (response.request().resourceType() !== 'image') return;
@@ -55,8 +68,6 @@ export default async function handler(
     });
   }).then(pageResponse => pageResponse.buffer());
   console.log("completely buffer has gotten: ", buffer);
-  await browser.close();
-  console.log("browser close completed");
 
   
   const all = fs.createWriteStream("out.jpg");
@@ -68,4 +79,7 @@ export default async function handler(
   res.setHeader('Content-Length', buffer.length);
   res.status(200).send(buffer);
   res.end();
+  */
+  await browser.close();
+  console.log("browser close completed");
 }
