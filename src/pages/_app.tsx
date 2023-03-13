fe5aef1ce3b31171855903f16df82d9d85c248a5
Sharlotte <aaa9810321@gmail.com>
2023-03-13 13:26:42 +0900

    feat: remove a tag default style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-// _app.tsx
 import type { AppProps } from "next/app";
 import CssBaseline from "@mui/material/CssBaseline";
+import "../../public/global.css";
 
 const App = (props: AppProps) => {
   const { Component, pageProps } = props;
