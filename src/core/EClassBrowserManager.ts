ea93ae6619d2b9d48e6717ce17f3de0b106f0473
Sharlotte <aaa9810321@gmail.com>
2023-03-13 11:34:20 +0900

    refactor: 클래스 분리

---
+++ src/core/EClassBrowserManager.ts
@@ -0,0 +1,42 @@
+import BrowserManager from "@/core/BrowserManager";
+import { load as loadCheerio } from "cheerio";
+
+const KU_ECLASS_LOGIN_URL =
+  "https://cyber.kyungnam.ac.kr/ilos/main/member/login_form.acl";
+
+class EClassBrowserManager extends BrowserManager {
+  public constructor(private id: string, private password: string) {
+    super();
+  }
+
+  public async start() {
+    await super.start();
+    await this.auth();
+
+    return this;
+  }
+
+  async auth() {
+    console.log("page changing...");
+    await this.currentPage.goto(KU_ECLASS_LOGIN_URL);
+    console.log("page change completed: ", this.currentPage.url());
+
+    await this.currentPage.focus("#usr_id");
+    await this.currentPage.keyboard.type(this.id);
+    await this.currentPage.focus("#usr_pwd");
+    await this.currentPage.keyboard.type(this.password);
+    await this.currentPage.click("#login_btn");
+    await this.currentPage.waitForNavigation({ timeout: 1000000000 });
+    console.log("page login completed: ", this.currentPage.url());
+  }
+
+  public async getMainPage() {
+    const content = await this.currentPage.content();
+    const $ = loadCheerio(content);
+    $("script, style").remove();
+    const wrap = $("#wrap");
+    return wrap;
+  }
+}
+
+export default EClassBrowserManager;
--- src/pages/api/crawel.ts
+++ src/pages/api/crawel.ts
@@ -1,47 +1,19 @@
-  import { type NextApiRequest, type NextApiResponse } from "next";
-  import puppeteer from "puppeteer";
-  import { load } from 'cheerio';
+import { type NextApiRequest, type NextApiResponse } from "next";
+import EClassBrowserManager from "@/core/EClassBrowserManager";
 
-  const KU_ECLASS_LOGIN_URL =
-    "https://cyber.kyungnam.ac.kr/ilos/main/member/login_form.acl";
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log("API requrest detected!");
 
-  export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-    console.log("API requrest detected!");
-    
-    const { id = process.env.ID, password = process.env.PASSWORD } = req.query as Record<string, string>;
-    if(!id || !password) throw new Error("you need id / password to get data!");
+  const { id = process.env.ID, password = process.env.PASSWORD } =
+    req.query as Record<string, string>;
+  if (!id || !password) throw new Error("you need id / password to get data!");
 
-    console.log("browser launching...");
-    const browser = await puppeteer.launch({
-      ignoreHTTPSErrors: true,
-      headless: true,
-      defaultViewport: {
-        width: 500,
-        height: 358,
-      },
-    });
-    console.log("browser launch completed");
-
-    const page = await browser.newPage();
-    await page.goto(KU_ECLASS_LOGIN_URL);
-    console.log("page change completed: ", page.url());
-
-    await page.focus("#usr_id");
-    await page.keyboard.type(id);
-    await page.focus("#usr_pwd");
-    await page.keyboard.type(password);
-    await page.click("#login_btn");
-    await page.waitForNavigation({ timeout: 1000000000 });
-    console.log("page login completed: ", page.url());
-
-    const content = await page.content();
-    const $ = load(content);
-    $("*").remove("script").remove("style");
-    console.log($("#wrap").toString());
-
-    await browser.close();
-    console.log("browser close completed");
-  }
+  const manager = await new EClassBrowserManager(id, password).start();
+  const wrap = await manager.getMainPage();
+  console.log(wrap.toString());
+  res.send(JSON.stringify({ content: wrap.toString() }));
+  manager.close();
+}
