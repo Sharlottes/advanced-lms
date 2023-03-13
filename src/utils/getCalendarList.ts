50413ec8131a1d4f79073780960d5a96ef98024e
Sharlotte <aaa9810321@gmail.com>
2023-03-13 23:03:39 +0900

    feat: next auth 추가

---
+++ src/utils/getCalendarList.ts
@@ -0,0 +1,8 @@
+export const getCalendarList = async () => {
+  const res = await fetch(
+    "https://www.googleapis.com/calendar/v3/users/me/calendarList"
+  );
+  if (!res.ok) throw new Error(res.statusText);
+  const data = await res.json();
+  console.log(data);
+};
