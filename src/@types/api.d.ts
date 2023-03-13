ae1d657d87f2578f176759e44d6cf69a92703925
Sharlotte <aaa9810321@gmail.com>
2023-03-13 11:37:07 +0900

    refactor: add api response type

---
+++ src/@types/api.d.ts
@@ -0,0 +1,3 @@
+interface CrawlResponse {
+  content: string;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage: React.FC = () => {
   const [html, setHtml] = React.useState<JSX.Element>();
   const handleClick = async () => {
-    const res = await fetch(`/api/crawl`)
+    const res: CrawlResponse = await fetch(`/api/crawl`)
       .then((res) => res.json())
       .catch(() => undefined);
     if (!res) return;
