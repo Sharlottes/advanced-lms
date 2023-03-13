264536f5b019c42df2955fad34181f50a2a32f5d
Sharlotte <aaa9810321@gmail.com>
2023-03-13 11:34:32 +0900

    feat: 크롤링 결과 가시화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 
 const HomePage: React.FC = () => {
+  const [html, setHtml] = React.useState<JSX.Element>();
   const handleClick = async () => {
-    const res = await fetch(`/api/getSchedule`)
-    if(!res.ok) return;
+    const res = await fetch(`/api/crawel`)
+      .then((res) => res.json())
+      .catch(() => undefined);
+    if (!res) return;
+    console.log(res.content);
+    setHtml(<div dangerouslySetInnerHTML={{ __html: res.content }}></div>);
   };
-  return <button onClick={handleClick}>go</button>;
+  return (
+    <>
+      <button onClick={handleClick}>go</button>
+      {html}
+    </>
+  );
 };
 
 export default HomePage;
