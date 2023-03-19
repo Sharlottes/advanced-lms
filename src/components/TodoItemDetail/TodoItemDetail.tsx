cc6b8fca8878648a488596a0788597c26c85ee2a
Sharlotte <seeseafood@naver.com>
2023-03-19 16:12:37 +0900

    refactor: todoItemDetail fetch hook에 매핑/알림 책임 전가

--- src/components/TodoItemDetail/TodoItemDetail.tsx
+++ src/components/TodoItemDetail/TodoItemDetail.tsx
@@ -1,7 +1,5 @@
-import Box from "@mui/material/Box";
 import Chip from "@mui/material/Chip";
 import Typography from "@mui/material/Typography";
-import { useSnackbar } from "notistack";
 import useFetchTodoItemDetail from "@/hooks/useFetchTodoItemDetail";
 import {
   TodoItemDetailContainer,
@@ -21,33 +19,11 @@ const TodoItemDetail: React.FC<TodoItemDetailProps> = ({
   classId,
   type,
 }) => {
-  const { enqueueSnackbar } = useSnackbar();
-  const { data: rawTodoDetailData } = useFetchTodoItemDetail(
+  const { data: todoDetailData } = useFetchTodoItemDetail(
     todoId,
     classId,
-    type,
-    () => {
-      enqueueSnackbar({
-        key: "todo_detail_too_long",
-        variant: "warning",
-        message: (
-          <div style={{ textAlign: "left" }}>
-            <strong>로딩이 생각보다 오래 걸립니다!</strong>
-            <br />
-            네트워크 상태가 좋지 않거나 비밀번호가 알맞지 않으면 데이터를 불러올
-            수 없습니다.
-            <br />
-            해당 사항을 점검하고 재시도해보십시오.
-          </div>
-        ),
-      });
-    }
+    type
   );
-
-  const todoDetailData = Object.fromEntries(rawTodoDetailData) as Record<
-    DetailType,
-    string
-  >;
 
   return (
     <TodoItemDetailContainer>
