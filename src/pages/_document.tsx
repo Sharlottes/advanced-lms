81a7e4f454c3f77f8baa9e7edca1d4bf67498957
Sharlotte <aaa9810321@gmail.com>
2023-03-20 12:35:13 +0900

    feat: add lang attribute to html tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ import { ServerStyleSheets } from "@mui/styles";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="ko">
         <Head />
         <body>
           <Main />
