3760b92463ca8b93b7799ee2bfa69a5f6db13c73
Sharlotte <aaa9810321@gmail.com>
2024-09-11 16:23:00 +0900

    feat: 팝업 수거 버튼 추가

---
+++ contents/PopupCollectButton.tsx
@@ -0,0 +1,53 @@
+import type { PlasmoCSConfig } from "plasmo"
+import { useEffect, useState } from "react"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://cyber.kyungnam.ac.kr/*"],
+  all_frames: true
+}
+const PopupCollectButton = () => {
+  const [data, setData] = useState<[string, string][]>([])
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const mainPop = document.getElementsByClassName("mainpop")[0]
+    const elements = mainPop
+      .querySelectorAll(
+        "div:nth-of-type(2) > div:nth-of-type(2) > div:nth-of-type(2) > span > p:has(a) > a"
+      )
+      .values() as IterableIterator<HTMLAnchorElement>
+
+    for (const element of elements) {
+      const uri = element.href
+      const imgUri = element.getElementsByTagName("img")[0].src
+      console.log(uri)
+      setData((prev) => [...prev, [uri, imgUri] as const])
+    }
+    mainPop.remove()
+  }, [])
+
+  return (
+    <div
+      style={{
+        zIndex: 999,
+        position: "fixed"
+      }}>
+      <button onClick={() => setIsOpen((prev) => !prev)}>
+        팝업 {isOpen ? "닫기" : "열기"}
+      </button>
+      {isOpen && (
+        <div style={{ display: "flex", gap: "8px", flexWrap: "wrap" }}>
+          {data.map(([uri, imgUri]) => (
+            <div key={uri}>
+              <a href={uri}>
+                <img src={imgUri} width={360} />
+              </a>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default PopupCollectButton
