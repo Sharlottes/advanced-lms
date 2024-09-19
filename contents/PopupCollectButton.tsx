a59af4a66ea143cc18adc5d50928c3007ecde2ee
Sharlotte <aaa9810321@gmail.com>
2024-09-19 11:05:32 +0900

    fix: 이미지 밖에서 클릭됨 / 한 링크에 여러 이미지 예외처리

--- contents/PopupCollectButton.tsx
+++ contents/PopupCollectButton.tsx
@@ -14,15 +14,16 @@ export default function PopupCollectButton() {
   useEffect(() => {
     const mainPop = document.getElementsByClassName("mainpop")[0];
     const elements = mainPop
-      .querySelectorAll(
-        "div:nth-of-type(2) > div:nth-of-type(2) > div:nth-of-type(2) > span > p:has(a) > a"
-      )
+      .querySelectorAll(".mainpop span > p:has(a) > a")
       .values() as IterableIterator<HTMLAnchorElement>;
 
     for (const element of elements) {
       const uri = element.href;
-      const imgUri = element.getElementsByTagName("img")[0].src;
-      setData((prev) => [...prev, [uri, imgUri] as const]);
+      const imgElems = Array.from(element.getElementsByTagName("img"));
+      setData((prev) => [
+        ...prev,
+        ...imgElems.map((e) => [uri, e.src] as [string, string])
+      ]);
     }
     mainPop.remove();
   }, []);
@@ -30,31 +31,50 @@ export default function PopupCollectButton() {
   return (
     <div
       style={{
+        width: "100%",
+        height: "100%",
         zIndex: 999,
-        position: "absolute",
         inset: 0
       }}>
       <button
         onClick={() => setIsOpen((prev) => !prev)}
         style={{
-          textWrap: "nowrap"
+          textWrap: "nowrap",
+          zIndex: 9991,
+          position: "relative"
         }}>
         팝업 {isOpen ? "닫기" : "열기"}
       </button>
       {isOpen && (
-        <div
-          style={{
-            display: "flex",
-            gap: "8px",
-            flexWrap: "wrap",
-            width: "calc(100vw - 18px)"
-          }}>
-          {data.map(([uri, imgUri]) => (
-            <a href={uri} key={uri}>
-              <img src={imgUri} width={360} />
-            </a>
-          ))}
-        </div>
+        <>
+          <div
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              position: "fixed",
+              inset: 0,
+              zIndex: 9990
+            }}
+            onClick={() => setIsOpen(false)}
+          />
+          <div
+            style={{
+              display: "flex",
+              gap: "8px",
+              flexWrap: "wrap",
+              width: "calc(100vw - 18px)",
+              margin: "16px",
+              position: "relative",
+              zIndex: 9991
+            }}>
+            {data.map(([uri, imgUri]) => (
+              <div key={uri}>
+                <a href={uri}>
+                  <img src={imgUri} width={360} />
+                </a>
+              </div>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
